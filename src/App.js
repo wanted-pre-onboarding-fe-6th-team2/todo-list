f16c62587f8f4cc66baa3586c35ac870d46eb0e7
nknkcho <srirachacho@gmail.com>
2022-08-31 00:53:12 +0900

    Env: reset css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import styled from '@emotion/styled';
-
-const Button = styled.button`
-  padding: 32px;
-  background-color: hotpink;
-  font-size: 24px;
-  border-radius: 4px;
-  color: black;
-  font-weight: bold;
-  &:hover {
-    color: white;
-  }
-`;
+import { Global } from '@emotion/react';
+import resetCss from './styles/global';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <Button>This my button component.</Button>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
+  <div>
+    <Global styles={resetCss}></Global>
   </div>
 );
 
