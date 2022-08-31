02204f1730c12e94a7cd84b5d9b2deadff6a5617
Ha Sung Hwa <tjdghk3920@naver.com>
2022-08-31 21:23:20 +0900

    Fix: useInput 검증 로직 변경
    type을 나누지 않고 include를 인자로 받아 포함여부 확인

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,31 +1,25 @@
 import { useState, useCallback, useRef } from 'react';
 
 export const useInput = options => {
-  const { initialValue, minLength = 0, type = 'string' } = options || {};
+  const { initialValue, minLength = 0, include = '' } = options || {};
   const [value, setValue] = useState(initialValue || '');
   const isValid = useRef(false);
 
   const handleString = useCallback(
     receivedValue => {
+      if (include) {
+        isValid.current = receivedValue && receivedValue.includes(include);
+      }
       isValid.current = receivedValue.length >= minLength;
       setValue(receivedValue);
     },
-    [minLength]
+    [minLength, include]
   );
-
-  const handleEmail = useCallback(receivedValue => {
-    isValid.current = receivedValue && receivedValue.includes('@');
-    setValue(receivedValue);
-  }, []);
 
   const onChangeInput = useCallback(
     e => {
       const targetValue = e.target.value || '';
-      if (type === 'email') {
-        handleEmail(targetValue);
-      } else {
-        handleString(targetValue);
-      }
+      handleString(targetValue);
     },
     [type, handleEmail, handleString]
   );
