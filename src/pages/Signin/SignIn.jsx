3298ccfb3dc83fdae3556099c9de58e1aa81c07c
Ha Sung Hwa <tjdghk3920@naver.com>
2022-08-31 21:24:42 +0900

    Fix: handleSignIn function 분리(SignInForm로 이동)

--- src/pages/Signin/SignIn.jsx
+++ src/pages/Signin/SignIn.jsx
@@ -3,26 +3,9 @@ import SignInForm from 'components/Auth/SignInForm/SignInForm';
 import { Container } from './SignIn.styled';
 
 const SignInPage = () => {
-  const handleSignIn = async (e, email, password) => {
-    e.preventDefault();
-    const signInResponse = await AuthApiService.signIn(email, password);
-    if (signInResponse.status === 200) {
-      // 토큰 등록
-      localStorage.setItem('token', signInResponse.accessToken);
-      // 리다이렉트
-      //navigate('/todo');
-    } else if (signInResponse.status === 404) {
-      // 404 사용자 존재x
-      alert('존재하지 않는 사용자 입니다.');
-    } else if (signInResponse.status === 401) {
-      // 401 비밀번호 틀림
-      alert('사용자 정보가 일치하지 않습니다.');
-    }
-  };
-
   return (
     <Container>
-      <SignInForm submitHandler={handleSignIn} />
+      <SignInForm />
     </Container>
   );
 };
