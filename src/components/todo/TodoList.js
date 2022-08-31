a23ef18a14e918022422ef3c65fe9f185e4d8703
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-31 20:26:36 +0900

    Feat: getTodos 로직 구현

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,37 +1,20 @@
 import React, { useState, useEffect } from 'react';
-// import { useTodo } from 'hooks/useTodo';
 import * as Styled from 'styles/todo/todo.styled';
 import TodoItem from './TodoItem';
-
-const dummy = [
-  {
-    id: 1,
-    todo: '과제하기',
-    isCompleted: true,
-    userId: 1,
-  },
-  {
-    id: 2,
-    todo: '동료학습 하기',
-    isCompleted: false,
-    userId: 1,
-  },
-];
+import TodoApiService from 'api/todos';
 
 const TodoList = () => {
-  // const [getTodos] = useTodo();
-
   const [todos, setTodos] = useState([]);
 
-  // console.log(getTodos());
+  const token = localStorage.getItem('token') || '';
 
   useEffect(() => {
-    // setTodos(getTodos());
-    setTodos(dummy);
-  }, []);
+    TodoApiService.getTodos({ accessToken: token }).then(response => setTodos(response));
+  }, [token]);
 
   return (
     <Styled.Ul>
+      {todos && todos.length === 0 && <h3>No Tasks.</h3>}
       {todos &&
         todos.map(todo => {
           return <TodoItem key={todo.id} todo={todo} />;
