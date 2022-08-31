c4ede106b3db21b62cf02a2069240891b1d1c20c
yeongjuyun <myyun1029@gmail.com>
2022-08-31 19:32:21 +0900

    Refactor: regEmail를 validateEmail함수 밖으로 분리

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,6 +1,7 @@
+const regEmail =
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
 const validateEmail = email => {
-  const regEmail =
-    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
   return regEmail.test(email);
 };
 
