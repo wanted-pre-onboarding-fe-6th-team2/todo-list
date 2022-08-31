12e1f6f152ce1c09d13afb603bca298fc01b8671
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-08-31 16:06:26 +0900

    Feat: SignInForm component 추가 및 스타일 적용

---
+++ src/components/Auth/SignInForm/SignInForm.jsx
@@ -0,0 +1,40 @@
+import { useInput } from 'hooks/useInput';
+import * as Styled from './SignInForm.styled';
+
+const SignInForm = ({ submitHandler }) => {
+  const [email, onChangeEmail, isValidEmail] = useInput({
+    initialValue: '',
+    minLength: 1,
+    type: 'email',
+  });
+
+  const [password, onChangePassword, isValidPassword] = useInput({
+    initialValue: '',
+    minLength: 8,
+  });
+
+  return (
+    <Styled.SignLayoutWrapper onSubmit={e => submitHandler(e, email, password)}>
+      <h3>로그인</h3>
+      <Styled.InputWrapper>
+        <Styled.Input type="text" value={email} onChange={onChangeEmail} />
+        {!isValidEmail && email.length > 0 && <p>@을 포함해 주세요.</p>}
+      </Styled.InputWrapper>
+      <Styled.InputWrapper>
+        <Styled.Input type="password" value={password} onChange={onChangePassword} />
+        {!isValidPassword && password.length > 0 && <p>8자리 이상 입력해 주세요.</p>}
+      </Styled.InputWrapper>
+      {isValidEmail && isValidPassword ? (
+        <Styled.SubmitBtn type="submit" isAllow={true}>
+          로그인
+        </Styled.SubmitBtn>
+      ) : (
+        <Styled.SubmitBtn isAllow={false} disabled>
+          로그인
+        </Styled.SubmitBtn>
+      )}
+    </Styled.SignLayoutWrapper>
+  );
+};
+
+export default SignInForm;
