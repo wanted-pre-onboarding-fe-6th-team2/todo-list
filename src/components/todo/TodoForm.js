d03965fa6a4bf1c8f2dab223867d236bb276689c
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-31 23:06:40 +0900

    Refactor: Todo 추가 시 input 초기화 추가

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import * as Styled from 'styles/todo/todo.styled';
 import TodoApiService from 'api/todos';
 
 const TodoForm = () => {
   const [newTodo, setNewTodo] = useState('');
 
+  const inputRef = useRef();
+
   const accessToken = localStorage.getItem('token') || '';
 
   const handleAddClick = e => {
     e.preventDefault();
 
-    newTodo && TodoApiService.createTodo({ todo: newTodo, accessToken });
-    setNewTodo('');
+    if (newTodo) {
+      TodoApiService.createTodo({ todo: newTodo, accessToken });
+      inputRef.current.value = '';
+    } else {
+      alert('할 일을 입력해주세요.');
+    }
   };
 
   return (
     <Styled.Form>
       <Styled.Input
         type="text"
+        ref={inputRef}
         placeholder="할 일을 작성해 주세요"
         onChange={e => setNewTodo(e.target.value)}
       />
