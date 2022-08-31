4935bf112cc9def4a72f6ef647bb0af661061863
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-31 20:52:39 +0900

    Refactor: getTodos 로직 변경

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,14 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import * as Styled from 'styles/todo/todo.styled';
-
 import TodoList from 'components/todo/TodoList';
 import TodoForm from 'components/todo/TodoForm';
+import TodoApiService from 'api/todos';
 
-const Todo = () => (
-  <Styled.Container>
-    <h1>Todo</h1>
-    <TodoForm />
-    <TodoList />
-  </Styled.Container>
-);
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token') || '';
+
+    TodoApiService.getTodos({ accessToken: token }).then(response => setTodos(response));
+  }, [todos]);
+
+  return (
+    <Styled.Container>
+      <h1>Todo</h1>
+      <TodoForm />
+      <TodoList todos={todos} />
+    </Styled.Container>
+  );
+};
 
 export default Todo;
