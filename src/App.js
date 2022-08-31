f43d2ba036f295260926fedffa660f36222df5cb
HaJunRyu <hjr4557@naver.com>
2022-08-31 11:55:27 +0900

    Feat: '/', 'todos' route에 해당하는 컴포넌트를 렌더링 시켜준다

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { Global } from '@emotion/react';
+import { ROUTES } from 'constants/route';
+import Home from 'pages/Home/Home';
+import Todos from 'pages/Todos/Todos';
+import { Route, Routes } from 'react-router-dom';
 import resetCss from 'styles/global';
 
 const App = () => (
   <div>
-    <Global styles={resetCss}></Global>
+    <Global styles={resetCss} />
+    <Routes>
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.TODOS} element={<Todos />} />
+    </Routes>
   </div>
 );
 
