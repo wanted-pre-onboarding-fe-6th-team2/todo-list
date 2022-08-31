fefb2ebd00197f4800ee21d1990fdebe2fe2a149
HaJunRyu <hjr4557@naver.com>
2022-08-31 14:49:45 +0900

    Feat: Signin, Signup 컴포넌트 생성하고 각각 알맞은 라우트에 매칭해준다

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Global } from '@emotion/react';
 import { ROUTES } from 'constants/route';
 import Home from 'pages/Home/Home';
+import Signin from 'pages/Signin/Signin';
+import Signup from 'pages/Signup/Signup';
 import Todos from 'pages/Todos/Todos';
 import { Route, Routes } from 'react-router-dom';
 import resetCss from 'styles/global';
@@ -11,6 +13,8 @@ const App = () => (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.TODOS} element={<Todos />} />
+      <Route path={ROUTES.SIGNIN} element={<Signin />} />
+      <Route path={ROUTES.SIGNUP} element={<Signup />} />
     </Routes>
   </div>
 );
--- src/constants/route.js
+++ src/constants/route.js
@@ -1,4 +1,6 @@
 export const ROUTES = {
   HOME: '/',
   TODOS: '/todos',
+  SIGNIN: '/signin',
+  SIGNUP: '/signup',
 };
