ad2c858583880b9f723401a55d026766b20d7b55
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-31 20:25:41 +0900

    Feat: createTodo 로직 구현

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from 'styles/todo/todo.styled';
+import TodoApiService from 'api/todos';
 
 const TodoForm = () => {
+  const [newTodos, setNewTodos] = useState('');
+
+  const token = localStorage.getItem('token') || '';
+
+  const handleAddClick = e => {
+    e.preventDefault();
+
+    newTodos && TodoApiService.createTodo({ todo: newTodos, accessToken: token });
+  };
+
   return (
     <Styled.Form>
-      <Styled.Input type="text" placeholder="할 일을 작성해 주세요" />
-      <Styled.Button>추가</Styled.Button>
+      <Styled.Input
+        type="text"
+        placeholder="할 일을 작성해 주세요"
+        onChange={e => setNewTodos(e.target.value)}
+      />
+      <Styled.Button onClick={handleAddClick}>추가</Styled.Button>
     </Styled.Form>
   );
 };
