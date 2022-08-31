5b2878e8f5f355934cab0df8ef44d29727f8a0d9
yeongjuyun <myyun1029@gmail.com>
2022-08-31 19:28:56 +0900

    Refator: export 컨벤션에 맞게 수정

--- src/components/Signup.style.js
+++ src/components/Signup.style.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+const Container = styled.div`
   width: 380px;
   border: 1px solid black;
   border-radius: 10px;
@@ -8,47 +8,47 @@ export const Container = styled.div`
   background-color: white;
 `;
 
-export const Title = styled.h2`
+const Title = styled.h2`
   font-size: 24px;
   text-align: center;
   padding-bottom: 30px;
 `;
 
-export const Form = styled.form`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
 `;
 
-export const Text = styled.div`
+const Text = styled.div`
   margin-top: 20px;
   font-weight: 400;
 `;
 
-export const LabelBox = styled.div`
+const LabelBox = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-export const Label = styled.label`
+const Label = styled.label`
   padding: 5px;
   font-weight: 500;
 `;
 
-export const ValidationText = styled.span`
+const ValidationText = styled.span`
   font-size: 12px;
   font-weight: 500;
   color: #666666;
 `;
 
-export const Input = styled.input`
+const Input = styled.input`
   height: 38px;
   padding: 0 10px;
   margin-bottom: 10px;
   font-size: 16px;
 `;
 
-export const Button = styled.button`
+const Button = styled.button`
   height: 38px;
   border: 0;
   border-radius: 8px;
@@ -65,3 +65,5 @@ export const Button = styled.button`
     pointer-events: none;
   }
 `;
+
+export { Container, Title, Form, Text, LabelBox, Label, ValidationText, Input, Button };
