0a84b9275d3ac3ddd99e76bfcc75fbf7dd7bed4e
jyoon <wldbszp@gmail.com>
2022-08-31 17:12:43 +0900

    App.js에서 작업코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Global } from '@emotion/react';
-import SignUp from 'pages/Signup/SignUp';
 import resetCss from 'styles/global';
 
 const App = () => (
   <div>
     <Global styles={resetCss}></Global>
-    <SignUp />
   </div>
 );
 
