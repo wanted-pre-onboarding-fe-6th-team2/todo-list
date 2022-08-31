88dc28571b4098ad229571f27e681a47de5623bd
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-31 21:21:20 +0900

    Refactor: deleteTodo, updateTodo 로직 코드 구조 변경

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -3,20 +3,50 @@ import * as Styled from 'styles/todo/todo.styled';
 import TodoApiService from 'api/todos';
 
 const TodoItem = ({ todo }) => {
-  const token = localStorage.getItem('token') || '';
+  const accessToken = localStorage.getItem('token') || '';
+
   const [editMode, setEditMode] = useState(false);
+  const [editTodo, setEditTodo] = useState('');
 
   const handleDelete = id => {
-    TodoApiService.deleteTodo({ accessToken: token, todoId: id });
+    TodoApiService.deleteTodo({ accessToken, todoId: id });
+  };
+
+  const handleComplete = todo => {
+    const completedTodo = {
+      ...todo,
+      isCompleted: !todo.isCompleted,
+    };
+
+    updateTodo(completedTodo);
+  };
+
+  const handleEdit = todo => {
+    const newTodo = {
+      ...todo,
+      todo: editTodo,
+    };
+
+    updateTodo(newTodo);
+    setEditMode(false);
+  };
+
+  const updateTodo = todo => {
+    TodoApiService.updateTodo({
+      accessToken,
+      isCompleted: todo.isCompleted,
+      todo: todo.todo,
+      todoId: todo.id,
+    });
   };
 
   return (
     <Styled.Li key={todo.id}>
       {editMode ? (
         <>
-          <Styled.Input defaultValue={todo.todo} />
+          <Styled.Input defaultValue={todo.todo} onChange={e => setEditTodo(e.target.value)} />
           <div>
-            <Styled.Button>수정</Styled.Button>
+            <Styled.Button onClick={() => handleEdit(todo)}>수정</Styled.Button>
             <Styled.Button onClick={() => setEditMode(!editMode)}>취소</Styled.Button>
           </div>
         </>
@@ -24,7 +54,7 @@ const TodoItem = ({ todo }) => {
         <>
           <Styled.Paragraph iscompleted={todo.isCompleted}>{todo.todo}</Styled.Paragraph>
           <div>
-            <Styled.Button>완료</Styled.Button>
+            <Styled.Button onClick={() => handleComplete(todo)}>완료</Styled.Button>
             <Styled.Button onClick={() => setEditMode(!editMode)}>수정</Styled.Button>
             <Styled.Button onClick={() => handleDelete(todo.id)}>삭제</Styled.Button>
           </div>
