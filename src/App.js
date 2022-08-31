d0d254ec793a4298dfce7fc2027d59d3a4a6dc83
nknkcho <srirachacho@gmail.com>
2022-08-31 15:31:44 +0900

    Feat: '/', '/todos', '/signup','/signin'에 해당하는 컴포넌트를 연결, 토큰 유무에 따른 리다이렉션 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { Global } from '@emotion/react';
+import { ROUTES } from 'constants/route';
+import Home from 'pages/Home/Home';
+import Signin from 'pages/Signin/Signin';
+import Signup from 'pages/Signup/Signup';
+import Todos from 'pages/Todos/Todos';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import resetCss from 'styles/global';
 
-const App = () => (
-  <div>
-    <Global styles={resetCss}></Global>
-  </div>
-);
+const App = () => {
+  // checkToken : 토큰 유무를 확인 (default : true)
+  const checkToken = !!localStorage.getItem('token');
+  return (
+    <Global styles={resetCss}>
+      <Routes>
+        <Route path={ROUTES.HOME} element={checkToken ? <Navigate to="/todo" /> : <Home />}></Route>
+        <Route path={ROUTES.TODOS} element={checkToken ? <Todos /> : <Navigate to="/" />}></Route>
+        <Route path={ROUTES.SIGNIN} element={<Signin />}></Route>
+        <Route path={ROUTES.SIGNUP} element={<Signup />}></Route>
+      </Routes>
+    </Global>
+  );
+};
 
 export default App;
