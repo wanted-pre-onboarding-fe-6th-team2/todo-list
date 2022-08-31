1da8c0ea327aeae0b330ecad9bcb15b56121dbb7
nknkcho <srirachacho@gmail.com>
2022-08-31 17:33:07 +0900

    Fix: 상수값으로 선언된 변수로 수정, 변수명 수정, Global 잘못 사용한 부분 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Global } from '@emotion/react';
 import { ROUTES } from 'constants/route';
+import { TOKEN } from 'constants/token';
 import Home from 'pages/Home/Home';
 import Signin from 'pages/Signin/Signin';
 import Signup from 'pages/Signup/Signup';
@@ -8,17 +9,21 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import resetCss from 'styles/global';
 
 const App = () => {
-  // checkToken : 토큰 유무를 확인 (default : true)
-  const checkToken = !!localStorage.getItem('token');
+  // hasToken : 토큰 유무를 확인 (default : true)
+  const hasToken = !!localStorage.getItem(TOKEN.KEY);
   return (
-    <Global styles={resetCss}>
+    <div>
+      <Global styles={resetCss} />
       <Routes>
-        <Route path={ROUTES.HOME} element={checkToken ? <Navigate to="/todo" /> : <Home />}></Route>
-        <Route path={ROUTES.TODOS} element={checkToken ? <Todos /> : <Navigate to="/" />}></Route>
+        <Route path={ROUTES.HOME} element={hasToken ? <Navigate to={ROUTES.TODOS} /> : <Home />} />
+        <Route
+          path={ROUTES.TODOS}
+          element={hasToken ? <Todos /> : <Navigate to={ROUTES.SIGNIN} />}
+        />
         <Route path={ROUTES.SIGNIN} element={<Signin />}></Route>
         <Route path={ROUTES.SIGNUP} element={<Signup />}></Route>
       </Routes>
-    </Global>
+    </div>
   );
 };
 
